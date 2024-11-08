55c55be28f0a896ef76ee5c8eeec5b0db06bab5b
PerrineLVB <perrine.launay@wanadoo.fr>
2024-11-08 11:37:32 +0100

    add methods in model

--- models/Pokemon.php
+++ models/Pokemon.php
@@ -1,21 +1,76 @@
 <?php
+require_once 'Database.php';
 class Pokemon {
     private $conn;
     private $table = 'pokemons';
 
-    public $id;
-    public $name;
-    public $type;
+    private $id;
+    private $name;
+    private $type;
 
-    public function __construct($db) {
-        $this->conn = $db;
+    public function getId() {
+        return $this->id;
+    }
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function getType() {
+        return $this->type;
+    }
+    public function setType($type) {
+        $this->type = $type;
     }
 
+
     public function getAllPokemons() {
+        $dbh = $this->conn;
         $query = "SELECT id, name, type FROM " . $this->table;
-        $stmt = $this->conn->prepare($query);
+        $stmt = $dbh->prepare($query);
         $stmt->execute();
         return $stmt;
     }
+
+    public function getPokemonById($id) {
+        $dbh = $this->conn;
+        $query = ("SELECT * FROM ". $this->table ." WHERE id_post = :id");
+        $stmt = $dbh->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Pokemon');
+        $pokemon = $stmt->fetch();
+        return $pokemon;
+    }
+
+    public function createPokemon() {
+        $dbh = $this->conn;
+        $query = "INSERT INTO " . $this->table . " (name, type) VALUES (:name, :type)";
+        $stmt = $dbh->prepare($query);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':type', $this->type);
+        $stmt->execute();
+    }
+
+    public function updatePokemon() {
+        $dbh = $this->conn;
+        $query = "UPDATE " . $this->table . " SET name = :name, type = :type WHERE id = :id";
+        $stmt = $dbh->prepare($query);
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':type', $this->type);
+        $stmt->execute();
+    }
+
+    public function deletePokemon($id) {
+        $dbh = $this->conn;
+        $query = "DELETE FROM ". $this->table ." WHERE id = :id";
+        $stmt = $dbh->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
 }
-?>
